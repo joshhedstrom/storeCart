f93b0abb942fff39a5d5416f3b54ef7302e9e37b
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-17 19:39:59 -0500

    configured server, wrote main handelbars view

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view-engine', 'handlebars');
+
+const routes = require('./controllers/controller.js')
+
+app.use(routes);
+
+app.listen(PORT, ()=>{
+	console.log(`Server listening on: http://localhost:${PORT}`);
+})
