831e5382f7279d64b5c1d31b0b240664ccb2aa60
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-17 20:41:04 -0500

    configured controller and cart.js

---
+++ controllers/controller.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+const store = require('../models/cart.js');
+
+router.post('/store', (req, res)=>{
+	//create
+})
+
+router.post('/cart', (req, res)=>{
+	//create
+})
+
+
+router.get('/', (req, res)=>{
+	//read
+})
+
+
+router.put('/store/:id', (req, res)=>{
+	//update
+})
+
+router.put('/cart/:id', (req, res)=>{
+	//update
+})
+
+
+router.delete('/store/:id', (req, res)=>{
+	//delete
+})
+
+router.delete('/cart/:id', (req, res)=>{
+	//delete
+})
+
