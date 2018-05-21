3e8d872eeb3f7e5bfa989e93b428c113587b33f9
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-21 12:16:00 -0500

    got to load products on page

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,55 +3,47 @@ const router = express.Router();
 
 const store = require('../models/cart.js');
 
-const cb = function (res) {
-	console.log('callback function: ', res)
-}
+// const cb = function (res) {
+// 	console.log('callback function: ', res)
+// }
 
-router.post('/store', (req, res)=>{
-	//create
+router.post('/store', (req, res) => {
+    //create
 })
 
-router.post('/cart', (req, res)=>{
-	//create
-})
-
-
-router.get('/', (req, res)=>{
-	store.read('products', cb, (data)=>{
-		console.log('data1: ', data)
-		let hbsObject = {
-			items: data,
-		}
-		console.log(hbsObject)
-	});
-
-	store.read('cart', cb, (data)=>{
-		res.render('index.handlebars');
-		console.log('data2: ', data)
-		let hbsObject = {
-			items: data,
-		}
-		console.log(hbsObject)
-	});
-		res.render('index.handlebars');
+router.post('/cart', (req, res) => {
+    //create
 })
 
 
-router.put('/store/:id', (req, res)=>{
-	//update
+router.get('/', (req, res) => {
+    store.read('products', (data) => {
+        let items = {products: data};
+        res.render('index.handlebars', items)
+    });
+
+    store.read('cart', (data) => {
+        let items = {products: data};
+        res.render('index.handlebars', items)
+    });
 })
 
-router.put('/cart/:id', (req, res)=>{
-	//update
+
+router.put('/store/:id', (req, res) => {
+    //update
+})
+
+router.put('/cart/:id', (req, res) => {
+    //update
 })
 
 
-router.delete('/store/:id', (req, res)=>{
-	//delete
+router.delete('/store/:id', (req, res) => {
+    //delete
 })
 
-router.delete('/cart/:id', (req, res)=>{
-	//delete
+router.delete('/cart/:id', (req, res) => {
+    //delete
 })
 
 module.exports = router;
--- models/cart.js
+++ models/cart.js
@@ -1,32 +1,28 @@
 const orm = require('../config/orm.js');
-// let cb = function (res) {
-// 	console.log(res)
-// }
 
 const store = {
     create: (table, name, price, quantity, cb) => {
     	orm.create(table, name, price, quantity, (res)=>{
-    		// cb(res);
+    		cb(res);
     	});
     },
     read: (table, cb) => {
     	orm.read(table, (res)=>{
-    		console.log(res)
+    		cb(res);
     	});
     },
 
     update: (table, stock, item, cb) => {
     	orm.update(table, stock, item, (res)=>{
-    		// cb(res);
+    		cb(res);
     	});
     },
 
     delete: (table, id, cb) => {
     	orm.delete(table, id, (res)=>{
-    		// cb(res);
+    		cb(res);
     	});
     }
-
 }
 
 module.exports = store;
